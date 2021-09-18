03a6144c4097fea834903087ce8172a90481292e
Vanessa <vanessa-almeida@prodesan.com.br>
2021-09-18 19:09:59 -0300

    commit inicial

---
+++ src/main/java/digitalinnovation/example/app/client/JavaHttpClient.java
@@ -0,0 +1,87 @@
+package digitalinnovation.example.app.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import digitalinnovation.example.app.dto.MessageSend;
+import digitalinnovation.example.app.dto.ResultBotTelegram;
+import digitalinnovation.example.app.dto.ResultBotTelegramList;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Component
+public class JavaHttpClient {
+    @Value("${telegram.api}")
+    private String BASE_PATH;
+    @Value("${telegram.token}")
+    private String TOKEN;
+
+    private HttpClient httpClient;
+    private ObjectMapper objectMapper;
+
+    public JavaHttpClient(ObjectMapper objectMapper) {
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = objectMapper;
+    }
+
+    public ResultBotTelegramList buscarAtualizacao() {
+        ResultBotTelegramList resultBotTelegramList = null;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_PATH + TOKEN + "/getUpdates"))
+                .GET()
+                .build();
+
+        HttpResponse<String> stringHttpResponse = sendRequest(request);
+
+        try {
+            // pega a resposta e transforma no objeto ResultBotTelegramList por meio do objectMapper
+            resultBotTelegramList = objectMapper.readValue(stringHttpResponse.body(), ResultBotTelegramList.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultBotTelegramList;
+    }
+
+    public ResultBotTelegram enviarMensagem(MessageSend msg) {
+        ResultBotTelegram resultBotTelegram = null;
+
+        String mensagem = null;
+        try {
+            // transforma o objeto recebido "MessageSend" em um JSON
+            mensagem = objectMapper.writeValueAsString(msg);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_PATH + TOKEN + "/sendMessage"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(mensagem))
+                .build();
+
+        HttpResponse<String> stringHttpResponse = sendRequest(request);
+
+        try {
+            // transforma a resposta recebida em JSON no objeto ResultBotTelegram
+            resultBotTelegram = objectMapper.readValue(stringHttpResponse.body(), ResultBotTelegram.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultBotTelegram;
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) {
+        HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return httpResponse;
+    }
+}
---
+++ src/main/java/digitalinnovation/example/app/service/Telegram.java
@@ -0,0 +1,45 @@
+package digitalinnovation.example.app.service;
+
+import digitalinnovation.example.app.client.FeingClient;
+import digitalinnovation.example.app.client.JavaHttpClient;
+import digitalinnovation.example.app.client.RestTemplateClient;
+import digitalinnovation.example.app.dto.MessageSend;
+import digitalinnovation.example.app.dto.ResultBotTelegram;
+import digitalinnovation.example.app.dto.ResultBotTelegramList;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Telegram {
+    private RestTemplateClient restTemplateClient;
+    private FeingClient feingClient;
+    private JavaHttpClient javaHttpClient;
+
+    public Telegram(RestTemplateClient restTemplateClient, FeingClient feingClient, JavaHttpClient javaHttpClient) {
+        this.restTemplateClient = restTemplateClient;
+        this.feingClient = feingClient;
+        this.javaHttpClient = javaHttpClient;
+    }
+
+    public void enviarMensagem(MessageSend mensagem) {
+        //HTTP CLIENT
+        ResultBotTelegram resultBotTelegramResponseEntity = javaHttpClient.enviarMensagem(mensagem);
+
+        //RestTemplate
+        //restTemplateClient.enviarMensagem(mensagem);
+
+        //FeingClient
+        //ResultBotTelegram resultBotTelegram = feingClient.enviarMensagem1(mensagem);
+    }
+
+    public ResultBotTelegramList buscarAtualizacao() {
+        //HTTP CLIENT
+        ResultBotTelegramList resultBotTelegramList = javaHttpClient.buscarAtualizacao();
+
+        //RestTemplate
+        //ResultBotTelegramList resultBotTelegramList1 = restTemplateClient.buscarAtualizacao();
+
+        //FeingClient
+        //feingClient.buscaratualizacao();
+        return resultBotTelegramList;
+    }
+}
